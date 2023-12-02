cc7c0cc2f54c6f6cb4bd06da10538a8e4516ad1a
John Schlaak <john-schlaak@github.com>
2023-12-02 15:15:10 -0600

    added day 2b solution

--- src/day_2.rs
+++ src/day_2.rs
@@ -1,46 +1,115 @@
-pub fn sum_valid_game_ids(games: String, red_limit: u32, green_limit: u32, blue_limit: u32) -> u32 {
-    games.split('\n').map(
-        |game| get_id_from_game_if_valid(game.trim(), red_limit, green_limit, blue_limit)
-    ).sum()
+use std::cmp::max;
+
+struct Game {
+    id: u32,
+    grabs: Vec<Grab>
+}
+
+impl Game {
+    fn is_valid(&self, rgb_limits: (u32, u32, u32)) -> bool {
+        self.grabs.iter().all(
+            |grab| [(grab.red, rgb_limits.0), (grab.green, rgb_limits.1), (grab.blue, rgb_limits.2)].iter().all(
+                |(grab_color, limit)| grab_color.is_none() || grab_color.unwrap() <= *limit
+            )
+        )
+    }
+
+    fn get_minimum_cubes_grab(&self) -> Grab {
+        let [red, green, blue] = self.grabs.iter().map(
+            |grab| [grab.red, grab.green, grab.blue]
+        ).reduce(
+            |min_colors, colors| {
+                let result = min_colors.into_iter().zip(colors.into_iter()).map(
+                    |(min_color, color)| if let (Some(min_color), Some(color)) = (min_color, color) {
+                        Some(max(min_color, color))
+                    } else if min_color.is_none() {
+                        color
+                    } else {
+                        min_color
+                    }
+                ).collect::<Vec<Option<u32>>>();
+                [result[0], result[1], result[2]]
+            }
+        ).unwrap_or([None, None, None]);
+        Grab {red, green, blue}
+    }
+}
+
+struct Grab {
+    red: Option<u32>,
+    green: Option<u32>,
+    blue: Option<u32>
+}
+
+impl Grab {
+    fn get_power(&self) -> u32 {
+        self.red.unwrap_or(1) * self.green.unwrap_or(1) * self.blue.unwrap_or(1)
+    }
+}
+
+enum Color {
+    Red(u32),
+    Green(u32),
+    Blue(u32)
+}
+
+pub fn sum_valid_game_ids(games_string: String, rgb_limits: (u32, u32, u32)) -> u32 {
+    parse_games(games_string).iter().map(|game| if game.is_valid(rgb_limits) { game.id } else { 0 }).sum()
 }
 
 
-fn get_id_from_game_if_valid(game: &str, red_limit: u32, green_limit: u32, blue_limit: u32) -> u32 {
-    let colon_split = game.split(':').collect::<Vec<&str>>();
-    if let (Some(id_term), Some(grabs_term)) = (colon_split.get(0), colon_split.get(1)) {
-        let space_split = id_term.split_whitespace().collect::<Vec<&str>>();
-        let id = if let Some(id_str) = space_split.get(1) {
-            id_str.parse::<u32>().unwrap_or(0)
-        } else {
-            0
-        };
-        let valid = {
-            let grab_terms = grabs_term.split(';').collect::<Vec<&str>>();
-            grab_terms.iter().all(
-                |grab_term| {
-                    let color_terms = grab_term.split(',').map(|color_term| color_term.trim()).collect::<Vec<&str>>();
-                    color_terms.iter().all(
-                        |color_term| {
-                            let space_split = color_term.split_whitespace().collect::<Vec<&str>>();
-                            if let (Some(count_str), Some(color)) = (space_split.get(0), space_split.get(1)) {
-                                let count = count_str.parse::<u32>().unwrap_or(0);
-                                match *color {
-                                    "red" => count <= red_limit,
-                                    "green" => count <= green_limit,
-                                    "blue" => count <= blue_limit,
-                                    _ => false
+pub fn sum_powers_of_minimum_grabs(games_string: String) -> u32 {
+    parse_games(games_string).iter().map(|game| game.get_minimum_cubes_grab().get_power()).sum()
+}
+
+
+fn parse_games(games_string: String) -> Vec<Game> {
+    games_string.split('\n').map(
+        |game_string| {
+            if let Some([id_term, grabs_term]) = game_string.trim().split(':').collect::<Vec<&str>>().get(0..2) {
+                let id = if let Some(id_str) = id_term.split_whitespace().collect::<Vec<&str>>().get(1) {
+                    if let Ok(id) = id_str.parse::<u32>() {
+                        id
+                    } else {
+                        panic!("Could not parse id for game '{}'", game_string)
+                    }
+                } else {
+                    panic!("Could not parse id_str for game '{}'", game_string)
+                };
+                let grabs = grabs_term.split(';').map(
+                    |colors_term| {
+                        let colors = colors_term.trim().split(',').map(
+                            |color_term| if let Some([count_str, color_str]) = color_term.split_whitespace().collect::<Vec<&str>>().get(0..2) {
+                                let count = if let Ok(count) = count_str.parse::<u32>() {
+                                    count
+                                } else {
+                                    panic!("Could not interpret a count_str as a number for game '{}'", game_string)
+                                };
+                                match *color_str {
+                                    "red" => Color::Red(count),
+                                    "green" => Color::Green(count),
+                                    "blue" => Color::Blue(count),
+                                    _ => panic!("Could not interpret a color_str as 'red', 'green', or 'blue' for game '{}'", game_string)
                                 }
                             } else {
-                                false
+                                panic!("Could not parse a color_term for game '{}'", game_string)
+                            }
+                        );
+                        let mut grab = Grab {red: None, green: None, blue: None};
+                        for color in colors {
+                            match color {
+                                Color::Red(count) => grab.red = Some(grab.red.unwrap_or(0) + count),
+                                Color::Green(count) => grab.green = Some(grab.green.unwrap_or(0) + count),
+                                Color::Blue(count) => grab.blue = Some(grab.blue.unwrap_or(0) + count)
                             }
                         }
-                    )
-                }
-            )
-        };
-        if valid {
-            return id;
+                        grab
+                    }
+                ).collect::<Vec<Grab>>();
+                Game{id, grabs}
+            } else {
+                panic!("Could not parse id_term and grabs_term for game '{}'", game_string)
+            }
         }
-    }
-    return 0;
+    ).collect::<Vec<Game>>()
 }
--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,7 @@ use utilities::read_input_file;
 fn main() {
     println!("Day 1a: {}", day_1::sum_first_and_last_digits(read_input_file("day_1.txt"), false));
     println!("Day 1b: {}", day_1::sum_first_and_last_digits(read_input_file("day_1.txt"), true));
-    println!("Day 2a: {}", day_2::sum_valid_game_ids(read_input_file("day_2.txt"), 12, 13, 14));
+    
+    println!("Day 2a: {}", day_2::sum_valid_game_ids(read_input_file("day_2.txt"), (12, 13, 14)));
+    println!("Day 2b: {}", day_2::sum_powers_of_minimum_grabs(read_input_file("day_2.txt")));
 }
